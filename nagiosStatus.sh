2d5e6c4cd842892c3d7638ba801e6fb4aaaf0662
Hilary B. Brenum (MBP) <hilary@hilarybisenieks.com>
2018-11-05 14:30:23 -0800

    Added host checks as well as service checks

--- nagiosStatus.sh
+++ nagiosStatus.sh
@@ -53,6 +53,7 @@ rm -f statPage statPageLog
 servicePage="/nagios/cgi-bin/status.cgi?host=all&type=detail&hoststatustypes=3&serviceprops=42&servicestatustypes=28"
 hostPage="/nagios/cgi-bin/status.cgi?hostgroup=all&style=hostdetail&hoststatustypes=12&hostprops=42"
 options="-T 10 -t 4 -w 1 --retry-connrefused --server-response"
+statusPages="$servicePage $hostPage"
 
 # username and password for nagios user if server is secured with htpasswd
 user=""
@@ -66,48 +67,60 @@ statPageLog="statPageLog"
 critColor="red"
 warnColor="yellow"
 unkColor="orange"
+critical=0
+warning=0
+unknown=0
 
-# get status
-wget $options http://$host$page -O "$statPage" -o "$statPageLog" --user="$user" --password="$pass"
+nagCheck () {
+	# get status
+	wget $options http://$host$1 -O "$statPage" -o "$statPageLog" --user="$user" --password="$pass"
 
-# exit 1 if wget fails
-if [ $? != 0 ] ; then
-	echo "Unable to fetch staus info"
-	exit 1
+	# exit 1 if wget fails
+	if [ $? != 0 ] ; then
+		echo "Unable to fetch staus info"
+		exit 1
+	fi
+
+	# check for OK HTTP header
+	rcode=`grep "HTTP/1.1 200 OK" "$statPageLog" | awk '{ print $2 }'`
+
+	# if HTTP 200 OK, proceed, otherwise return HTTP status and exit
+	if [ "$rcode" -eq "200" ] ; then
+		# get numbers for critical, warning, and unknown statuses
+		c=`egrep  "BGCRITICAL'>(1/1|2/2|3/3|4/4)" "$statPage" | wc -l | sed -e 's/ //g'`
+		w=`egrep  "BGWARNING'>(1/1|2/2|3/3|4/4)" "$statPage" | wc -l | sed -e 's/ //g'`
+		u=`egrep "BGUNKNOWN" "$statPage" | wc -l | sed -e 's/ //g'`
+		critical=$((critical + c))
+		warning=$((warning + w))
+		unknown=$((unknown + u))
+	else
+		echo "Server returned HTTP $rcode"
+		exit 1
+	fi
+}
+
+for i in $statusPages ; do
+	nagCheck $i
+done
+
+# format nonzero numbers for problem statuses for use with xmobar
+if [ "$critical" -gt 0 ] ; then
+	cOut="<fc=$critColor>$critical</fc>"
+else
+	cOut="$critical"
 fi
 
-# check for OK HTTP header
-rcode=`grep "HTTP/1.1 200 OK" "$statPageLog" | awk '{ print $2 }'`
-
-# if HTTP 200 OK, proceed, otherwise return HTTP status and exit
-if [ "$rcode" -eq "200" ] ; then
-	# get numbers for critical, warning, and unknown statuses
-	critical=`egrep  "BGCRITICAL'>(1/1|2/2|3/3|4/4)" "$statPage" | wc -l | sed -e 's/ //g'`
-	warning=`egrep  "BGWARNING'>(1/1|2/2|3/3|4/4)" "$statPage" | wc -l | sed -e 's/ //g'`
-	unknown=`egrep "BGUNKNOWN" "$statPage" | wc -l | sed -e 's/ //g'`
-
-	# format nonzero numbers for problem statuses for use with xmobar
-	if [ "$critical" -gt 0 ] ; then
-		cOut="<fc=$critColor>$critical</fc>"
-	else
-		cOut="$critical"
-	fi
-
-	if [ "$warning" -gt 0 ] ; then
-		wOut="<fc=$warnColor>$warning</fc>"
-	else
-		wOut="$warning"
-	fi
-
-	if [ "$unknown" -gt 0 ] ; then
-		uOut="<fc=$unkColor>$unknown</fc>"
-	else
-		uOut="$unknown"
-	fi
-
-	echo "C:$cOut W:$wOut U:$uOut"
-	exit 0
+if [ "$warning" -gt 0 ] ; then
+	wOut="<fc=$warnColor>$warning</fc>"
+else
+	wOut="$warning"
 fi
 
-echo "Server returned HTTP $rcode"
-exit 1
+if [ "$unknown" -gt 0 ] ; then
+	uOut="<fc=$unkColor>$unknown</fc>"
+else
+	uOut="$unknown"
+fi
+
+echo "C:$cOut W:$wOut U:$uOut"
+exit 0
